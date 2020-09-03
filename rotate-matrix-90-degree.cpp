0ba2ad7dca2afd16d4505c777a943562953dd490
Gowri Jayaprakash <34152914+gowrijp@users.noreply.github.com>
2020-09-03 18:21:49 +0530

    Create rotate-matrix-90-degree.cpp

---
+++ rotate-matrix-90-degree.cpp
@@ -0,0 +1,28 @@
+/*https://leetcode.com/problems/rotate-image/
+
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+Example 3:
+
+Input: matrix = [[1]]
+Output: [[1]]
+Example 4:
+
+Input: matrix = [[1,2],[3,4]]
+Output: [[3,1],[4,2]]
+*/
+
+void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int a = 0;
+        int b = n-1;
+        while(a<b){
+            for(int i=0;i<(b-a);++i){
+                swap(matrix[a][a+i], matrix[a+i][b]);
+                swap(matrix[a][a+i], matrix[b][b-i]);
+                swap(matrix[a][a+i], matrix[b-i][a]);
+            }
+            ++a;
+            --b;
+        }
+    }
